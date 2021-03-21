3d2d44807800fad056f9fca8b0db85ddc44ee5ae
Navan Sami <navan.sami@yahoo.com>
2021-03-21 20:45:31 +0200

    redirected data fetch from backend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const products = require('./data/products');
 
 const app = express();
 
-app.get('/' ,(req, res) => {
+app.get('/api/products' ,(req, res) => {
     res.json(products);
 })
 
-app.get('/products/:id' ,(req, res) => {
-    const product = products.find(p => p._id === req.params.id)
+app.get('/api/products/:id' ,(req, res) => {
+    const product = products.find(p => p._id ===cd req.params.id)
+    console.log("inside get by id");
     res.json(product);
 })
 
