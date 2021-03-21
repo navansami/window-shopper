4c612d2dcc805c5eda6199bc0c8b71f403a8947a
Navan Sami <navan.sami@yahoo.com>
2021-03-21 19:41:21 +0200

    installed express and created basic server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/' ,(req, res) => {
+    res.json(products);
+})
+
+app.get('/products/:id' ,(req, res) => {
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product);
+})
+
+app.listen(5000, console.log('server started on port 5000'));
